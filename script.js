88ec8ed7dae96116fb8292b7159512c3b1b38f3a
Rakibul6090 <oobilash161@gmail.com>
2025-02-09 23:54:17 +0600

    test js

---
+++ script.js
@@ -0,0 +1,19 @@
+let test;
+
+test = 6;
+
+ if(test>=60){ 
+    console.log("The ticket price:$8");
+}
+
+else if(test>=18){ 
+    console.log("The ticket price:$15");
+}
+
+else if(test>=5){ 
+    console.log("The ticket price:$10");
+}
+
+else if(test<5){
+    console.log("The ticket is free");
+}
